fcc98d49908567a4d53ffabe551b2fd8495bcaf9
datnvn <idolsupperman3215@gmail.com>
2022-08-02 10:54:57 +0700

    add datjava

---
+++ java/dat.java
@@ -0,0 +1,19 @@
+package demo-paracel.java;
+
+public class dat {
+    static int x = 10;
+    public static void show()
+    {
+        System.out.println(x);
+    }
+    public static void main(String args[]) {
+        Temp t = new Temp(); 
+        t.show(); 
+        Temp t1 = new Temp(); 
+        t1.x = 20;
+        t1.show();
+        System.out.println(x);
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
